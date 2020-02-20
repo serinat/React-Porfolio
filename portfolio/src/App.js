c805a942317b31417c0e08420a60be580a175105
Serina Tsang <serinat@uw.edu>
2020-02-20 03:28:55 -0800

    added landing page and animation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@
 import React from 'react';
 // Import the BrowserRouter, Route and Link components
 import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
@@ -17,7 +18,8 @@ function App() {
       <div className="App">
         <Navbar />
 
-        <Route exact path="/" component={About} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
 
---
+++ portfolio/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './animate.css';
+
+function Home(props) {
+    return (
+        <div>
+            <h1 id="me" class="animated slideInLeft">Serina Tsang ~ Not your ordinary lab rat who aspires to please, tidy and
+        build everyday.</h1>
+
+        </div>
+    )
+}
+
+export default Home;
