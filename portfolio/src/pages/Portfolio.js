e18599cd9cb514cf340cf90793a96b383cf67b28
Serina Tsang <serinat@uw.edu>
2020-02-22 12:36:16 -0800

    added project links

--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -9,19 +9,24 @@ function Portfolio(props) {
   return (
     <div>
       <h1 style={{ margin: 20 }}>Portfolio</h1>
+      <a href="https://github.com/BSmalls6/Project_1"> [Blackjack Github]</a>
+      <a href="https://bsmalls6.github.io/Project_1/"> [Blackjack Deployed]</a>
       <Thumbnail
         link="https://bsmalls6.github.io/Project_1/"
         image="./blackjack.png"
         
-        title="Blackjack"
-        category="Project"
+        //title="Blackjack"
+        //category="Project"
       />
+
+      <a href="https://github.com/cbarcala23/Project2"> [Lounge Github]</a>
+      <a href="https://serene-headland-87883.herokuapp.com/"> [Lounge Deployed]</a>
 
       <Thumbnail
         link="https://serene-headland-87883.herokuapp.com"
         image="./lounge.png"
-        title="The Lounge"
-        category="Project"
+        //title="The Lounge"
+        //category="Project"
       />
     </div>
   )
