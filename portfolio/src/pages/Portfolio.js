fa658d8c87cae98153750bd9ff1e72247a7ea2fd
Serina Tsang <serinat@uw.edu>
2020-02-22 10:35:38 -0800

    updated readme

--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import Blackjack from './blackjack.png';
-import Lounge from './lounge.png';
+import Thumbnail from './Thumbnail';
+//import './App.css';
+//import Blackjack from './blackjack.png';
+//import Lounge from './lounge.png';
 //import Project from './components/Project';
- 
+
 function Portfolio(props) {
   return (
     <div>
       <h1 style={{ margin: 20 }}>Portfolio</h1>
-      <br></br>
-      <a href="https://bsmalls6.github.io/Project_1/">[Project 1: Blackjack]</a>
-      <br></br>
-      <img src={Blackjack} alt=""/>
-      <br></br>
-      <a href="https://serene-headland-87883.herokuapp.com/">[Project 2: The Lounge]</a>
-      <br></br>
-      <img src={Lounge} alt=""/>
-    </div>  
+      <Thumbnail
+        link="https://bsmalls6.github.io/Project_1/"
+        image="./blackjack.png"
+        
+        title="Blackjack"
+        category="Project"
+      />
 
+      <Thumbnail
+        link="./lounge"
+        image="https://serene-headland-87883.herokuapp.com"
+        title="The Lounge"
+        category="Project"
+      />
+    </div>
   )
 }
- 
+
 export default Portfolio;
