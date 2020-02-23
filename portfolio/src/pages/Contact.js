21d2de627ad9412ec2a5944c21cd4c6ab5ac96b8
Serina Tsang <serinat@uw.edu>
2020-02-23 00:07:22 -0800

    updates

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -3,22 +3,30 @@ import React from "react"
 function Contact(props) {
   return (
     <div style={{ margin: 20 }}>
-      <div><h1>Contact + Resources </h1>
-        <br></br>
-        <p class="float-left"><i class="far fa-envelope fa-2x animated rotateIn"
-        ></i><a href="mailto:serinat@uw.edu">  [serinat@uw.edu]</a></p>
-
-        <p class="float-left" ><i
-          class="fas fa-mobile-alt fa-2x animated rotateIn"></i>  (425) 301-2906
-    </p>
-        <p class="float-left" ><i
-          class="far fa-file-pdf fa-2x animated rotateIn"></i><a href="Tsang_Serina_resume November 2019.pdf" download>  [Resume]</a>
-        </p>
-        <p class="float-right" ><i
-          class="fab fa-github fa-2x animated rotateIn"></i><a href="https://github.com/serinat">  [Github]</a></p>
-        <p class="float-right" ><i
-          class="fab fa-linkedin fa-2x animated rotateIn"></i><a
-            href="https://www.linkedin.com/in/serina-tsang-91b810105/">  [LinkedIn]</a></p></div>
+      <h1>Contact + Resources </h1>
+      <br></br>
+      <div class="row">
+        <div class="col-md-4">
+          <p class="float-left"><i class="far fa-envelope fa-2x animated rotateIn"
+          ></i><a href="mailto:serinat@uw.edu">  [serinat@uw.edu]</a></p></div>
+        <div class="col-md-4">
+          <p class="float-left" ><i
+            class="fas fa-mobile-alt fa-2x animated rotateIn"></i>  (425) 301-2906
+    </p></div>
+        <div class="col-md-4">
+          <p class="float-left" ><i
+            class="far fa-file-pdf fa-2x animated rotateIn"></i><a href="Tsang_Serina_resume November 2019.pdf" download>  [Resume]</a>
+          </p></div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <p class="float-right" ><i
+            class="fab fa-github fa-2x animated rotateIn"></i><a href="https://github.com/serinat">  [Github]</a></p></div>
+        <div class="col-md-6">
+          <p class="float-right" ><i
+            class="fab fa-linkedin fa-2x animated rotateIn"></i><a
+              href="https://www.linkedin.com/in/serina-tsang-91b810105/">  [LinkedIn]</a></p></div>
+      </div>
     </div>
 
   );
